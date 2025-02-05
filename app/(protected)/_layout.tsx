e4b379605f84d304dcbfc6368ae802e14d19c951
Johann Marzolla <email@fiap.com.br>
2025-02-05 19:40:19 -0300

    Adicionando navegação Drawer ao projeto

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,14 +1,43 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { View, Text } from 'react-native';
 import { Slot } from 'expo-router';
 import { useAuth } from '@/context/AuthContext';
 
-export default function ProtectedLayout() {
+const Drawer = createDrawerNavigator();
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+
+export default function App() {
   const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
-    return null;
+    return null; // ou redirecionar para uma tela de login
   }
 
+  return (
+    <Drawer.Navigator >
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Profile" component={ProfileScreen} />
+      <Drawer.Screen name="Protected" component={ProtectedLayout} />
+    </Drawer.Navigator>
+  );
+}
+
+function ProtectedLayout() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
       <Text style={{ fontSize: 18, marginBottom: 16 }}>Protected Layout</Text>
--- firebase/config.ts
+++ firebase/config.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
     storageBucket: "bytebank-8af58.firebasestorage.app",
     messagingSenderId: "491378965758",
     appId: "1:491378965758:web:7479b4e53821e6059cb9af"
-};
+  };
 
 let app: FirebaseApp
 let auth: Auth
